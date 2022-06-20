839855ec3989a9f108d55c995b5862eb5af41c37
tetarchus <8436118+tetarchus@users.noreply.github.com>
2022-06-20 21:11:11 +0100

    Updates default map options

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,16 @@ const DEFAULT_CENTER = { lat: 0, lng: 0 };
 const DEFAULT_ZOOM = 5;
 const DEFAULT_MAP_OPTIONS: google.maps.MapOptions = {
   center: DEFAULT_CENTER,
+  streetViewControl: false,
+  rotateControl: true,
+  mapTypeControl: false,
+  styles: [
+    {
+      featureType: 'poi',
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }],
+    },
+  ],
   zoom: DEFAULT_ZOOM,
 };
 
